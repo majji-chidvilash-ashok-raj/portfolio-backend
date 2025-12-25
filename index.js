785b3de3a6364836f3e23612325760490d567e10
majji-chidvilash-ashok-raj <chidvilashmajji@gmail.com>
2025-12-25 17:10:11 +0530

    cors updated

--- index.js
+++ index.js
@@ -16,20 +16,28 @@ const PORT = process.env.PORT || 3000;
 
 
 const allowedOrigins = [
-  "https://portfolio-chidvilashmajji-admin.netlify.app/",
+  "https://portfolio-chidvilashmajji-admin.netlify.app",
   "https://portfolio-chidvilashmajji.netlify.app",
   "http://localhost:5173",
   "http://localhost:3000"
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
 }));
 
-app.use(cors());
+
 
 
 
